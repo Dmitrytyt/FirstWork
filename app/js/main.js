82ffd2ea7f8fe14bb50120945a04332c9d6f3f77
Dmitry <dmitrytyt@ya.ru>
2015-10-05 00:12:01 +0400

    Add upload form. rm cahed file. Add new files.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-	//console.log('Файл main.js загружен!');
+    $( '.add-project' ).on('click', function(e) {
+        e.preventDefault();
+
+        $( '.pop-content' ).bPopup({
+            contentContainer: '.pop-content',
+            loadUrl: 'upload-form.html',
+            modalColor: '#58697a'
+        });
+    });
 });
