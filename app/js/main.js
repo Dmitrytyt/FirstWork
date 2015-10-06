61204306625fa5d8a6b67b968bd91582df755355
Dmitry <dmitrytyt@ya.ru>
2015-10-07 02:39:44 +0400

    Added form uload project

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ $(function(){
         e.preventDefault();
 
         $( '.pop-content' ).bPopup({
+            closeClass: 'pr-close',
             contentContainer: '.pop-content',
             loadUrl: 'upload-form.html',
             modalColor: '#58697a'
